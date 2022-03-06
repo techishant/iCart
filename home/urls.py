094d6594c1303e1dd936466e6ecbb68a8836ab69
techishant <ishantsh12234@gmail.com>
2022-03-06 19:03:18 +0530

    Compleed template - Added accountsPage, cartPage, productDetails page

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.homePage, name="homePage"),
-    path('products', views.productPage, name="productsPage")
+    path('products', views.productPage, name="productsPage"),
+    path('account', views.accountPage, name="accountPage"),
+    path('cart', views.cartPage, name='cartPage'),
+    path('products/<str:slug>', views.productDetailPage, name="productDetailPage")
 ]
--- home/views.py
+++ home/views.py
@@ -6,3 +6,13 @@ def homePage(request):
 
 def productPage(request):
     return render(request, 'products.html')
+
+def accountPage(request):
+    return render(request, 'account.html')
+
+def cartPage(request):
+    return render(request, 'cart.html')
+
+def productDetailPage(request, slug):
+    print(slug)
+    return render(request, 'productDetails.html')
