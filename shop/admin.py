8a843a80c645ccb10e141b2cff87b33de8120323
techishant <ishantsh12234@gmail.com>
2022-03-06 14:55:46 +0530

    Starting all over agin

--- shop/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from shop.models import *
-
-# Register your models here.
-admin.site.register(product)
--- shop/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from shop import views
-
-urlpatterns = [
-    path('', views.shopHome, name="shopHome"),
-    path('add', views.addProd, name="addProd"),
-    path('checkout', views.checkout, name="checkout"),
-    path('<str:slug>', views.productDetail, name="productDetail")
-]
