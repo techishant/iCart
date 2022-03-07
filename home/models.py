ce37a8860767f46ab79617f0fc3a23d22c07e0d3
techishant <ishantsh12234@gmail.com>
2022-03-07 15:45:57 +0530

    Not able to do much, Really need to learn new stuffs :'(

--- home/models.py
+++ home/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    # ratings = ratings
+    price = models.PositiveIntegerField()
+
+    
+
+
+# SHIRT_SIZES = (
+#         ('S', 'Small'),
+#         ('M', 'Medium'),
+#         ('L', 'Large'),
+#     )
