67fe16b08b2085d244cbaf5ae221697b1db43e6e
techishant <ishantsh12234@gmail.com>
2022-03-07 18:14:42 +0530

    Constructed model of Product. Still in progress

--- home/models.py
+++ home/models.py
@@ -1,10 +1,38 @@
 from django.db import models
 
+RATINGS = (
+        (1, 'Poor'),
+        (2, 'Average'),
+        (3, 'Good'),
+        (4, 'Very Good'),
+        (5, 'Excellent')
+    )
+
+CATEGORIES = (
+    ('Fa','Fashion'),
+    ('El','Electronics'),
+    ('M&A','Mobiles & Accesories'),
+    ('C&A','Computers & Accesories'),
+    ('Bo','Books'),
+    ('Dec','Decors')   
+)
+
+LABEL = (
+    ('New', 'New'),
+    ('Featured', 'Featured')
+)
+
 # Create your models here.
 class Product(models.Model):
     title = models.CharField(max_length=255)
-    # ratings = ratings
-    price = models.PositiveIntegerField()
+    category = models.CharField(max_length=180, choices=CATEGORIES)
+    price = models.FloatField()
+    dis_image =  models.ImageField(upload_to='images/', default=None)
+    sub_images = models.ImageField(upload_to='images/', default=None)
+    desc = models.TextField()
+
+    def __str__(self):
+        return f"{self.category} \ {self.title}"
 
     
 
--- iCart/settings.py
+++ iCart/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -128,3 +128,7 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# MEDIA URL
+
+MEDIA_ROOT =  BASE_DIR / "media"
+MEDIA_URL = '/media/'
